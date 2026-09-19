ce0b4fa05b17bba58a59d8e253c3fee57ea24cb5
Doritos0 <meyesblue@gmail.com>
2026-09-19 16:22:32 -0300

    Agregar metodo de suma, tipado de variables y println, sintaxis basica del lenguaje

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,16 @@ mod config;
 fn main() {
     let _engine = config::db::get_engine();
     println!("Conexion a SQLite en rust :)");
+    println!("Se realizará prueba de metodo sumar");
+    let numero_1: i32 = 8;
+    let numero_2: i32 = 92;
+    let resultado: i32 = suma(numero_1, numero_2);
+    println!("Al resultado de sumar estos numeros es {resultado}");
+}
+
+fn suma(numero_1: i32, numero_2: i32) -> i32{
+    println!("Se entrega el siguiente numero 1: {numero_1}");
+    println!("Se entrega el siguiente numero 2: {numero_2}, se sumaran");
+    let total: i32 = &numero_1 + &numero_2;
+    return total;
 }
